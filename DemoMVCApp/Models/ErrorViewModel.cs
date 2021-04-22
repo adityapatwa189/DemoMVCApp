c28e0952491f36254c989d6b998267eafbfedc42
Aditya Patwa <aditya.patwa189@outlook.com>
2021-04-22 11:08:13 +0530

    Add project files.

---
+++ DemoMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
